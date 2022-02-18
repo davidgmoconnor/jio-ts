4a7738141f3f201a53a92fea502585ce3db16bed
David O'Connor <davidoconnor@Davids-MacBook-Pro.local>
2022-02-18 15:26:03 +0000

    show compiler warning

--- src/examples/string.errors.ts
+++ src/examples/string.errors.ts
@@ -7,10 +7,15 @@ const UserSchema = schema({
 });
 
 type User = toType<typeof UserSchema>;
+type ExtendedUser = User & { password: string };
 
 const validateUser = validate(UserSchema);
 
 function useUser(user: User): void {
+  console.log(user);
+}
+
+function useExtendedUser(user: ExtendedUser): void {
   console.log(user);
 }
 
@@ -22,3 +27,4 @@ const { errors, values } = validateUser({
 
 if (errors) console.log(JSON.stringify(errors)); // ["Expecting string at something but instead got: undefined","Expecting string at username but instead got: \"me\" (minimum length 8)"]
 if (values) useUser(values); // will print blank as values is undefined
+useExtendedUser(values); // compiler warning
